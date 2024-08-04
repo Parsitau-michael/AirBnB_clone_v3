1eab12cfdbcc571148d7c0ef3da7360c60902b70
Parsitau-michael <parsitaumichael@gmail.com>
2024-08-04 13:39:08 -0700

    Started my API and added a route that returns the status of the API

---
+++ api/v1/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" This module represents the driver for our application
+It sets up all the blueprints.
+"""
+
+from api.v1.views import app_views
+from flask import Flask
+from models import storage
+import os
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_appcontext():
+    """ Closes the storage session after each request."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host=os.getenv('HBNB_API_HOST', "0.0.0.0"),
+            port=int(os.getenv('HBNB_API_PORT', 5000)),
+            threaded=True)
